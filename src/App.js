168cc41e62b1d08827cf5a30d3c6c2f057b96011
erfan <erfanmhdipoor@gmail.com>
2023-01-24 20:39:27 +0330

    app added without jsx

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react"
+const App=()=>{ return React.createElement("div",
+{id:"main"},
+"hello") }
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM  from "react-dom";
- const element =React.createElement("div",
- {id:"main"},
- "hello")
-ReactDOM.render(element,document.getElementById('root'))
+import App from "./App";
+
+ReactDOM.render(<App/>,document.getElementById('root'))
